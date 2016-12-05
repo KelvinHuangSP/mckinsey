a221888743727dc328470a3841325940779722f5
Kelvin Huang <kelvin.huang@systempartners.com>
2016-12-05 17:14:05 +1100

    Add UserList button - All and Incomplete

--- app/src/app.js
+++ app/src/app.js
@@ -7,8 +7,9 @@ angular.module( 'mk-app', ['ngMaterial', 'mk.login', 'mk.users'])
           .icon("google_plus", "./assets/svg/google_plus.svg", 24)
           .icon("hangouts", "./assets/svg/hangouts.svg", 24)
           .icon("twitter", "./assets/svg/twitter.svg", 24)
-          .icon("phone", "./assets/svg/phone.svg", 24);
-          
+          .icon("phone", "./assets/svg/phone.svg", 24)
+          .icon("search", "./assets/svg/search.svg", 24);
+
         $mdThemingProvider.theme('default').primaryPalette('brown').accentPalette('red');
 
 
@@ -21,4 +22,4 @@ angular.module( 'mk-app', ['ngMaterial', 'mk.login', 'mk.users'])
         });
 
         $urlRouterProvider.otherwise('/login');
-    }); 
+    });
